1c22097a01329dd437dfa57496bfeb97ffda46bf
michelezoncheddu <michelezoncheddu@users.noreply.github.com>
2019-12-19 15:05:30 +0100

    Code refactoring and cleaning

--- src/main.py
+++ src/main.py
@@ -3,7 +3,11 @@ from csv import reader
 from network import Network
 
 
+TESTING_MODE = False
+
+
 def main():
+    """The main function."""
     features_cardinality = [3, 3, 2, 3, 4, 2]  # See data/monk.names file.
     features_values = sum(features_cardinality)
     features = len(features_cardinality)  # Number of features.
@@ -11,13 +15,13 @@ def main():
     training_set_path = 'data/training/monks-1.train'
     test_set_path = 'data/test/monks-1.test'
 
-    hidden_units = 3
+    hidden_units = 4
     output_units = 1
 
     nn = Network([features_values, hidden_units, output_units])
 
     training_set = []
-        
+
     with open(training_set_path, 'r') as file:
         file_reader = reader(file, delimiter=' ')  # File parsing.
         for line in file_reader:
@@ -32,30 +36,30 @@ def main():
 
             training_set.append(inputs)
 
-    for i in range(500):
-        nn.backpropagation(training_set)
-    
-    return
+    # Training.
+    for i in range(300):
+        nn.train(training_set)
 
-    errors = 0
+    if TESTING_MODE:
+        errors = 0
 
-    with open(test_set_path, 'r') as file:
-        file_reader = reader(file, delimiter=' ')  # File parsing.
-        for line in file_reader:
-            inputs = [0] * (features_values + 1)  # + 1 for class attribute.
-            inputs[0] = int(line[0])  # Saving class attribute.
-            offset = 0  # Offset inside encoded array.
+        with open(test_set_path, 'r') as file:
+            file_reader = reader(file, delimiter=' ')  # File parsing.
+            for line in file_reader:
+                inputs = [0] * (features_values + 1)  # + 1 for class attribute.
+                inputs[0] = int(line[0])  # Saving class attribute.
+                offset = 0  # Offset inside encoded array.
 
-            # One-hot encoding.
-            for i in range(features):
-                inputs[int(line[i + 1]) + offset] = 1
-                offset += features_cardinality[i]
-            
-            result = nn.predict(inputs[1:])
-            if round(result[0]) != int(line[0]):
-                errors += 1
-        
-        print(errors)
+                # One-hot encoding.
+                for i in range(features):
+                    inputs[int(line[i + 1]) + offset] = 1
+                    offset += features_cardinality[i]
+
+                result = nn.predict(inputs[1:])
+                if round(result[0]) != int(line[0]):
+                    errors += 1
+
+            print(errors)
 
 
 if __name__ == '__main__':
--- src/network.py
+++ src/network.py
@@ -1,6 +1,5 @@
 import math
 import numpy as np
-import random
 from scipy.special import expit
 
 
@@ -25,28 +24,30 @@ class Network:
 
         self.weights = [
             np.random.uniform(-value, value, (size[i], size[i - 1]))
-                for i in range(1, len(size))  # For every layer, without the input one.
+            for i in range(1, len(size))  # For every layer, without the input one.
         ]
 
-        self.biases =  [np.random.uniform(-value, value, size[i]) for i in range(1, len(size))]
-        
-        self.nets =    [np.zeros(size[i]) for i in range(1, len(size))]
+        self.biases = [np.random.uniform(-value, value, size[i]) for i in range(1, len(size))]
+
+        self.nets = [np.zeros(size[i]) for i in range(1, len(size))]
         self.outputs = [np.empty(size[i]) for i in range(1, len(size))]
 
-        self.deltas =  [np.empty(size[i]) for i in range(1, len(size))]
-    
+        self.deltas = [np.empty(size[i]) for i in range(1, len(size))]
+        self.gradients = []
+
     @staticmethod
     def activation_function(x):
         """Sigmoidal logistic function"""
         return expit(x)
-    
+
     @staticmethod
     def derivative(x):
         """Derivative of sigmoidal function (using the differential equation)"""
-        fx = expit(x)
-        return fx * (1.0 - fx)
-    
-    def feedforward(self, x):
+        f_x = expit(x)
+        return f_x * (1.0 - f_x)
+
+    def forward_propagation(self, x):
+        """Runs the neural network."""
         for i in range(len(self.weights)):  # For every layer.
             self.nets[i] = np.dot(
                 self.weights[i],
@@ -54,42 +55,51 @@ class Network:
             ) + self.biases[i]
             self.outputs[i] = self.activation_function(self.nets[i])
 
-    def backpropagation(self, training_set):
-        square_error = 0
-        misclassifications = 0
+    def back_propagation(self, inputs, error):
+        """Performs the backpropagation algorithm."""
+        # Output layer deltas.
+        self.deltas[-1] = error * self.derivative(self.nets[-1])
 
-        gradients = [
+        # Hidden units deltas.
+        for i in reversed(range(len(self.weights) - 1)):
+            self.deltas[i] = np.dot(
+                self.deltas[i + 1],
+                self.weights[i + 1]
+            ) * self.derivative(self.nets[i])
+
+        # Gradient computation.
+        for i in reversed(range(len(self.weights))):
+            self.gradients[i] += self.deltas[i].reshape(-1, 1) \
+                * (inputs if i == 0 else self.outputs[i - 1])
+
+    def train(self, training_set):
+        """Trains the neural network (batch mode)."""
+        square_error = 0
+
+        self.gradients = [
             np.zeros((self.size[i], self.size[i - 1]))
-                for i in range(1, len(self.size))  # For every layer, without the input one.
+            for i in range(1, len(self.size))  # For every layer, without the input one.
         ]
 
         for pattern in training_set:
-            self.feedforward(pattern[1:])
+            inputs = pattern[1:]
+            targets = pattern[:1]
 
-            error = pattern[:1] - self.outputs[-1]
+            self.forward_propagation(inputs)
+
+            error = targets - self.outputs[-1]
             square_error += math.pow(np.sum(error), 2)
 
-            # Output layer deltas.
-            self.deltas[-1] = error * self.derivative(self.nets[-1])
+            self.back_propagation(inputs, error)
 
-            # Hidden units deltas.
-            for i in reversed(range(len(self.weights) - 1)):
-                self.deltas[i] = np.dot(
-                    self.deltas[i + 1],
-                    self.weights[i + 1]
-                ) * self.derivative(self.nets[i])
-             
-            # Gradient computation.
-            for i in reversed(range(len(self.weights))):
-                gradients[i] += self.deltas[i].reshape(-1, 1) * (pattern[1:] if i == 0 else self.outputs[i - 1])
-           
         # Bias and weights update.
         self.biases = self.deltas
         for i in range(len(self.weights)):
-            self.weights[i] += self.LEARNING_RATE * gradients[i]
-            
+            self.weights[i] += self.LEARNING_RATE * self.gradients[i]
+
         print(square_error)
 
     def predict(self, inputs):
-        self.feedforward(inputs)
+        """Calculates the neural network output."""
+        self.forward_propagation(inputs)
         return self.outputs[-1]
